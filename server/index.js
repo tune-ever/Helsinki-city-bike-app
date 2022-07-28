c9c37c8ea690f0af470970c55ab181b8fb4aca16
Andreas Achte <andreas.achte@gmail.com>
2022-07-28 22:35:02 +0300

    create-react-app

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const http = require('http')
 const app = require('./app')
 const server = http.createServer(app)
+const axios = require('axios')
+const baseUrl = 'http://localhost:3003/api/journeys'
 
 
 server.listen(3003, () => {
